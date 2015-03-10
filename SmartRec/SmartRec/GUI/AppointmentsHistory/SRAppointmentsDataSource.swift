7767a752206c5d65e2e4a7f83a5d2f73438e69d3
Artsiom Karseka <artsiom_karseka@epam.com>
2015-03-10 09:52:56 +0300

    Added comments.

--- SmartRec/SmartRec/GUI/AppointmentsHistory/SRAppointmentsDataSource.swift
+++ SmartRec/SmartRec/GUI/AppointmentsHistory/SRAppointmentsDataSource.swift
@@ -17,6 +17,110 @@ protocol SRAppointmentsDataSourceProtocol {
     func numberOfItemInSection(index: Int) -> Int;
         
     func rebuildDataSet();
+    
+//    PARENT
+    
+    /*
+    @interface LGIDataSource : NSObject
+    
+    @property (weak, nonatomic) IBOutlet id<LGIDataSourceDelegate> delegate;
+    
+    
+    /**
+    Data source's objects. Default implementation returns empty array.
+    */
+    - (NSArray *)dataSet;
+    
+    /**
+    Tells the receiver to invalidate current data and rebuild the data set basing on a data stored in a local store. Default implementation does nothing.
+    */
+    - (void)rebuildDataSet;
+    
+    /**
+    Tells the receiver to load a data, put the data into a local store, and invalidate the data set. Default implementation does nothing.
+    */
+    - (void)refreshDataSet;
+    
+    @end
+    */
+    
+    /*
+    - (NSArray *)dataSet {
+    
+    return nil;
+    }
+    
+    - (void)rebuildDataSet {
+    }
+    
+    
+    - (void)refreshDataSet {
+    }
+
+    */
+    
+    /*
+    @interface LGICoreDataDataSource : LGIDataSource
+    
+    - (NSManagedObjectContext *)context {
+    
+    return [LGICoreDataContextProvider mainManagedObjectContext];
+    }
+
+    - (void)refreshDataSet
+    {
+    
+    [self.fetchedResultsController performFetch:NULL];
+    
+    if ([self.delegate respondsToSelector:@selector(dataSourceDidChangeDataSet:)]) {
+    
+    [self.delegate dataSourceDidChangeDataSet:self];
+    }
+    }
+    
+    - (void)rebuildDataSet {
+    
+    NSFetchedResultsController *fetchedResultsController = nil;
+    NSFetchRequest *fetchRequest = [self fetchRequest];
+    
+    if (self.fetchLimit) {
+    fetchRequest.fetchLimit = self.fetchLimit;
+    }
+    
+    if (fetchRequest) {
+    
+    fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:[self context] sectionNameKeyPath:[self sectionNameKeyPath] cacheName:nil];
+    
+    [fetchedResultsController performFetch:NULL];
+    
+    fetchedResultsController.delegate = self;
+    }
+    
+    self.fetchedResultsController = fetchedResultsController;
+    
+    
+    }
+    
+    /**
+    The managed object context the data source works with.
+    */
+    - (NSManagedObjectContext *)context;
+    
+    /**
+    The fetch request which is used to get the objects. The fetch request must have at least one sort descriptor.
+    */
+    - (NSFetchRequest *)fetchRequest;
+    
+    
+    @property (nonatomic) NSInteger fetchLimit;
+    
+    
+    
+    @property (nonatomic, readonly) NSFetchedResultsController *fetchedResultsController;
+    
+    @end
+    */
+
 }
 
 protocol SRAppointmentsDataSourceDelegate {
